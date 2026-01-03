03304ab1bdc710aabeb84e18f6e061163c69af82
Karel Nagel <nagelkarel@gmail.com>
2026-01-03 15:01:20 +0200

    db start

---
+++ api/db/schema.ts
@@ -0,0 +1,10 @@
+import { sql } from "drizzle-orm"
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core"
+
+export const users = sqliteTable("users", {
+	id: integer("id").primaryKey({ autoIncrement: true }),
+	email: text("email").notNull().unique(),
+	createdAt: integer("created_at", { mode: "timestamp" })
+		.notNull()
+		.default(sql`(unixepoch())`),
+})
