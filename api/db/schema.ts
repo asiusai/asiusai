194f8b0429633a129d9ddb13343e86bd1a95d119
Karel Nagel <nagelkarel@gmail.com>
2026-01-03 15:45:57 +0200

    users devices and routes

--- api/db/schema.ts
+++ api/db/schema.ts
@@ -1,10 +1,90 @@
-import { sql } from "drizzle-orm"
-import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core"
+import { relations } from 'drizzle-orm'
+import { integer, sqliteTable, text, real } from 'drizzle-orm/sqlite-core'
+import { Permission } from '../../connect/src/types'
 
-export const users = sqliteTable("users", {
-	id: integer("id").primaryKey({ autoIncrement: true }),
-	email: text("email").notNull().unique(),
-	createdAt: integer("created_at", { mode: "timestamp" })
-		.notNull()
-		.default(sql`(unixepoch())`),
+export const usersTable = sqliteTable('users', {
+  id: text('id').primaryKey(),
+  email: text('email').notNull().unique(),
+  regdate: integer('regdate', { mode: 'timestamp' }).notNull(),
+  superuser: integer('superuser', { mode: 'boolean' }).default(false),
+  user_id: text('user_id').notNull(),
+  username: text('username'),
 })
+
+export const devicesTable = sqliteTable('devices', {
+  dongle_id: text('dongle_id').primaryKey(),
+  public_key: text('public_key').notNull().unique(),
+
+  alias: text('alias'),
+  device_type: text('device_type'),
+  ignore_uploads: text('ignore_uploads'),
+  openpilot_version: text('openpilot_version'),
+
+  serial: text('serial'),
+  imei: text('imei'),
+  imei2: text('imei2'),
+})
+
+export const deviceUsersTable = sqliteTable('device_users', {
+  user_id: text('user_id').notNull(),
+  dongle_id: text('dongle_id'),
+  permission: text('permission').$type<Permission>(),
+})
+
+export const routesTable = sqliteTable('routes', {
+  fullname: text('fullname').primaryKey(),
+  create_time: integer('create_time', { mode: 'timestamp' }).notNull(),
+  dongle_id: text('dongle_id').notNull(),
+  end_lat: real('end_lat'),
+  end_lng: real('end_lng'),
+  end_time: text('end_time'),
+  git_branch: text('git_branch'),
+  git_commit: text('git_commit'),
+  git_commit_date: text('git_commit_date'),
+  git_dirty: integer('git_dirty', { mode: 'boolean' }),
+  git_remote: text('git_remote'),
+  is_public: integer('is_public', { mode: 'boolean' }).notNull(),
+  is_preserved: integer('is_public', { mode: 'boolean' }).notNull(),
+  distance: real('distance'),
+  maxqlog: real('maxqlog').notNull(),
+  platform: text('platform'),
+  procqlog: real('procqlog').notNull(),
+  start_lat: real('start_lat'),
+  start_lng: real('start_lng'),
+  start_time: text('start_time'),
+  url: text('url'),
+  version: text('version'),
+  vin: text('vin'),
+  make: text('make'),
+  id: real('id'),
+  car_id: real('car_id'),
+  version_id: real('version_id'),
+})
+
+// RELATIONS
+export const usersRelations = relations(usersTable, ({ many }) => ({
+  devices: many(deviceUsersTable),
+}))
+
+export const devicesRelations = relations(devicesTable, ({ many }) => ({
+  users: many(deviceUsersTable),
+  routes: many(routesTable),
+}))
+
+export const deviceUserRelations = relations(deviceUsersTable, ({ one }) => ({
+  user: one(usersTable, {
+    fields: [deviceUsersTable.user_id],
+    references: [usersTable.id],
+  }),
+  device: one(devicesTable, {
+    fields: [deviceUsersTable.dongle_id],
+    references: [devicesTable.dongle_id],
+  }),
+}))
+
+export const routesRelations = relations(routesTable, ({ one }) => ({
+  device: one(devicesTable, {
+    fields: [routesTable.dongle_id],
+    references: [devicesTable.dongle_id],
+  }),
+}))
--- api/router/device.ts
+++ api/router/device.ts
@@ -1,12 +1,23 @@
+import { eq } from 'drizzle-orm'
 import { contract } from '../../connect/src/api/contract'
 import { BadRequestError, NotImplementedError, randomId, tsr, verify } from '../common'
+import { db } from '../db/client'
+import { devicesTable } from '../db/schema'
 
 export const device = tsr.router(contract.device, {
-  register: async ({ query }) => {
-    const data = verify<{ register: boolean; exp: number }>(query.register_token, query.public_key)
+  register: async ({ query: { public_key, register_token, ...info } }) => {
+    const data = verify<{ register: boolean; exp: number }>(register_token, public_key)
     if (!data?.register) throw new BadRequestError()
-    
+
+    // Checking if device alread has registered
+    const device = await db.query.devicesTable.findFirst({ where: eq(devicesTable.public_key, public_key) })
+    if (device) {
+      await db.update(devicesTable).set(info).where(eq(devicesTable.dongle_id, device.dongle_id))
+      return { status: 200, body: { dongle_id: device.dongle_id } }
+    }
+
     const dongleId = randomId()
+    await db.insert(devicesTable).values({ dongle_id: dongleId, ...info, public_key })
     return { status: 200, body: { dongle_id: dongleId } }
   },
   getUploadUrl: async () => {
